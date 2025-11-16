21ab7f6a15278769958636d831f920fd2d14b158
infols2021-lab <info.ls.2021@gmail.com>
2025-11-16 17:05:20 +0300

    feat: smart configuration loading

--- supabase.js
+++ supabase.js
@@ -2,8 +2,8 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 // ЗАМЕНИ НА СВОИ РЕАЛЬНЫЕ КЛЮЧИ!
-const supabaseUrl = 'https://твой-проект.supabase.co'
-const supabaseAnonKey = 'твой_anon_ключ'
+const supabaseUrl = 'https://uyfhlpmugqgazedantwf.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5ZmhscG11Z3FnYXplZGFudHdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMyOTQ5MjcsImV4cCI6MjA3ODg3MDkyN30.wh4eazRMpyjNQ9unP3OmIt7f8e7P6vyyHWTlZEtgFFM'
 
 // Создаем клиент Supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
